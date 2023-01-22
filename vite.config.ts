551a8098757f62abd914f7f5678a209eeec13016
Sámal Rasmussen <samal84@gmail.com>
2023-01-22 01:08:32 +0000

    Try es5

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
-import { splitVendorChunkPlugin } from 'vite';
+// import { splitVendorChunkPlugin } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    // minify: false,
+    target: 'chrome58',
+    minify: false,
   },
   base: './',
-  plugins: [svelte(), splitVendorChunkPlugin()],
+  plugins: [
+    svelte(),
+    // splitVendorChunkPlugin()
+  ],
 });
